1c1c0008eeb730df65d5b474100903687a38b5cc
Abdullahi <thesomalidev@gmail.com>
2022-06-30 12:35:23 +0300

    Add Firebase user signin with email and password

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   TextInput,
   Button,
@@ -14,6 +15,11 @@ import {
 import { useForm } from "@mantine/form";
 import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import {
+  createUserFromAuth,
+  signinUserWithEmailAndPassword,
+  signInWithGooglePopup
+} from "../../utils/firebase";
 
 export default function Login() {
   const form = useForm({
@@ -23,15 +29,44 @@ export default function Login() {
     },
 
     validate: {
-      email: value => (/^\S+@\S+$/.test(value) ? null : "Invalid email")
+      username: value => value ? null : "Email or UserName is required"
     }
   });
+  const [isLoading, setLoading] = useState(false);
+  const [isGoogleLoading, setGoogleLoading] = useState(false);
 
-  const onSubmit = values => {
-    console.log(values);
+  const onSubmit = async e => {
+    e.preventDefault();
     const result = form.validate();
-    if (result.hasErrors) {
-      console.error(result.errors);
+    console.log(result.errors, form.values);
+    if (!result.hasErrors) {
+      try {
+        setLoading(true);
+        const { username: email, password } = form.values;
+        console.log(await signinUserWithEmailAndPassword(email, password));
+        setLoading(false);
+        form.reset()
+      } catch (error) {
+        setLoading(false);
+        console.error(error);
+        if(error.code === 'auth/wrong-password') {
+          form.setErrors({ password: 'Wrong Passwrod' });
+        } else if(error.code === 'auth/invalid-email') {
+          form.setErrors({ username: 'Invalid username or email.' });
+        }
+      }
+    }
+  };
+
+  const signupWithGoogle = async () => {
+    try {
+      setGoogleLoading(true);
+      const { user } = await signInWithGooglePopup();
+      await createUserFromAuth(user);
+      setGoogleLoading(false);
+    } catch (error) {
+      setGoogleLoading(false);
+      console.error(error);
     }
   };
 
@@ -59,7 +94,7 @@ export default function Login() {
       })}
     >
       <Container size="xs" px="xl">
-        <form onSubmit={form.onSubmit(onSubmit)}>
+        <form onSubmit={onSubmit}>
           <TextInput
             required
             label="Username"
@@ -74,7 +109,7 @@ export default function Login() {
           />
 
           <Group position="right" mt="md">
-            <Button type="submit" onClick={onSubmit}>
+            <Button type="submit" onClick={onSubmit} loading={isLoading}>
               Submit
             </Button>
           </Group>
@@ -87,7 +122,15 @@ export default function Login() {
         </form>
         <br />
         <Center>
-          <Button leftIcon={<FcGoogle />} variant="outline" size="lg" fullWidth>
+          <Button
+            leftIcon={<FcGoogle />}
+            variant="outline"
+            size="lg"
+            fullWidth
+            loading={isGoogleLoading}
+            loaderPosition="right"
+            onClick={signupWithGoogle}
+          >
             Continue with Google
           </Button>
         </Center>
